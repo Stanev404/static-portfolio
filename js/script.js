30232f44ae188bc1632b7cffff5ca01b50354e84
ivasta <zirow0000@gmail.com>
2021-10-24 14:54:35 +0300

    changed pen test heading

--- js/script.js
+++ js/script.js
@@ -79,7 +79,7 @@ $(document).ready(function () {
     }
 
     var typed = new Typed('.typewrite', {
-        strings: ["Java web developer.", "Cybersecurity enthusiast.", "Stockholm University graduate."],
+        strings: ["Penetration tester.", "Cybersecurity enthusiast.", "Stockholm University graduate."],
         loop: true,
         typeSpeed: 40,
         backSpeed: 40
